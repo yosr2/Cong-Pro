ffae6937df190e1532355542ada499270a30d476
yosr2 <yosr.laajimi@essths.u-sousse.tn>
2024-07-31 18:20:51 +0100

    favicon

--- register.php
+++ register.php
@@ -71,6 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <title>Inscription</title>
+    <link rel="icon" type="png" href="/assets/logo.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <style>
